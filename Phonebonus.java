072feac83ac01d49d15c3caa12b87c87430ed704
Uliana Utina <ulyana190909@yandex.ru>
2020-07-19 10:40:12 +0300

    исправление формулы рассчета бонусов

--- Phonebonus.java
+++ Phonebonus.java
@@ -1,11 +1,10 @@
 public class PhoneBonus {
     public static void main(String[] args) {
         long accountBalance = 300;
-        long refillAccount = 2500;
-        long totalAccount = accountBalance + refillAccount;
+        long refillAccount = 2100;
         int limit = 1000;
         int bonus;
-        if (totalAccount >= limit) {
+        if (refillAccount >= limit) {
             bonus = 1;
         } else {
             bonus = 0;
